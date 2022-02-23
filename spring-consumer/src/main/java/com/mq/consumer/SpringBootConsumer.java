131de82fb04d2a5114944e773a50e1998c1b928b
pb <iamapb@163.com>
2022-02-23 20:16:31 +0800

    fix

--- spring-consumer/src/main/java/com/mq/consumer/SpringBootConsumer.java
+++ spring-consumer/src/main/java/com/mq/consumer/SpringBootConsumer.java
@@ -1,12 +1,13 @@
-package com.mq.produce;
+package com.mq.consumer;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class SpringBooProduce {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringBootConsumer {
     public static void main(String[] args) {
-        new SpringApplication(SpringBooProduce.class).run(args);
+        new SpringApplication(SpringBootConsumer.class).run(args);
     }
 }
