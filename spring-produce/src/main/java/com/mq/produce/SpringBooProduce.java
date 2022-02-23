41355b3ac86f11650b4aec624f30e0224ee8cae7
pb <iamapb@163.com>
2022-02-23 20:07:39 +0800

    fix

---
+++ spring-produce/src/main/java/com/mq/produce/SpringBooProduce.java
@@ -0,0 +1,11 @@
+package com.mq.produce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBooProduce {
+    public static void main(String[] args) {
+        new SpringApplication(SpringBooProduce.class).run(args);
+    }
+}
