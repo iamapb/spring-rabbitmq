431af84fab958c94e4aee9363bef3e5c21b98db7
pb <iamapb@163.com>
2022-04-06 08:49:27 +0800

    fix

---
+++ java-base/src/main/java/com/base/leetCode/动态规划/动态规划最值.java
@@ -0,0 +1,16 @@
+package com.base.leetCode.动态规划;
+
+/**
+ * 动态规划问题的一般形式就是求最值。
+ * 动态规划其实是运筹学的一种最优化方法，只不过在计算机问题上应用比较多，
+ * 比如说让你求最长递增子序列呀，最小编辑距离呀等等
+ */
+public class 动态规划最值 {
+
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return 0;
+    }
+}
---
+++ java-base/src/main/java/com/base/leetCode/回溯算法/回溯算法解决全排列代码.java
@@ -0,0 +1,4 @@
+package com.base.leetCode.回溯算法;
+
+public class 回溯算法解决全排列代码 {
+}
