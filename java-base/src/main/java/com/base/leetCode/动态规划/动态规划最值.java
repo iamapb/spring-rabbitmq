00c13abc234886a9df8a49fa6a135eb400ef2ab4
pb <iamapb@163.com>
2022-04-06 14:15:05 +0800

    fix

--- java-base/src/main/java/com/base/leetCode/动态规划/动态规划最值.java
+++ java-base/src/main/java/com/base/leetCode/动态规划/动态规划最值.java
@@ -7,10 +7,21 @@ package com.base.leetCode.动态规划;
  */
 public class 动态规划最值 {
 
-    public int fib(int n) {
+    public static int fib(int n) {
+        int[] emo = new int[n + 1];
+        return helpNum(emo, n);
+
+    }
+    public static int helpNum(int[] emo, int n) {
         if (n == 0 || n == 1) {
             return n;
         }
-        return 0;
+        if (emo[n] != 0) return emo[n];
+        emo[n] = helpNum(emo, n -1) + helpNum(emo, n -2);
+        return emo[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(20));
     }
 }
--- java-base/src/main/java/com/base/leetCode/回溯算法/回溯算法解决全排列代码.java
+++ java-base/src/main/java/com/base/leetCode/回溯算法/回溯算法解决全排列代码.java
@@ -1,4 +1,40 @@
 package com.base.leetCode.回溯算法;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class 回溯算法解决全排列代码 {
+
+    private static List<List<Integer>> res = new ArrayList<>();
+    public static void main(String[] args) {
+        permute(new int[]{1,2,3});
+        System.out.println(JSON.toJSONString(res));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        boolean[] used = new boolean[nums.length];
+        tacktrack(nums, track, used);
+        return res;
+    }
+
+    public static void tacktrack(int[] nums, LinkedList<Integer> track, boolean[] used) {
+        if (nums.length == track.size()) {
+            res.add(new LinkedList<>(track));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            track.add(nums[i]);
+            used[i] = true;
+            tacktrack(nums, track, used);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
 }
