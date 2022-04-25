16dac2b99411d47bac555bec6af53b58e3ece159
pb <iamapb@163.com>
2022-04-25 14:37:45 +0800

    fix

--- spring-rocketmq/src/main/java/com/rocketmq/producer/SyncProducer.java
+++ spring-rocketmq/src/main/java/com/rocketmq/producer/SyncProducer.java
@@ -1,11 +1,8 @@
-package com.shardingjdbc.Producer;
+package com.rocketmq.producer;
 
-import org.apache.rocketmq.client.exception.MQBrokerException;
-import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
-import org.apache.rocketmq.remoting.exception.RemotingException;
 
 public class SyncProducer {
     public static void main(String[] args) throws Exception {
