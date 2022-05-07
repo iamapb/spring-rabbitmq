4eb62bb4ac44846138a9a83ffd7c41424f249554
panbin <iamapb@163.com>
2022-05-07 08:48:51 +0800

    fix

--- springboot-nacos/src/main/java/com/spring/nacos/SpringNacosApplication.java
+++ springboot-nacos/src/main/java/com/spring/nacos/SpringNacosApplication.java
@@ -4,9 +4,10 @@ package com.spring.nacos;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@NacosPropertySource(dataId = "spring", autoRefreshed = true)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@NacosPropertySource(dataId = "spring", autoRefreshed = true)
 public class SpringNacosApplication {
 
     public static void main(String[] args) {
